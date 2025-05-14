d94fa6657b8e2aff01bd6953147df02f7024d8e0
Sufyan03019777121 <156623479+Sufyan03019777121@users.noreply.github.com>
2025-05-14 07:22:11 +0500

    admin up111

--- backend/index.js
+++ backend/index.js
@@ -212,7 +212,7 @@ const phoneRoutes = require('./routes/phoneRoutes');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 app.use(cors());
