d9fb2047f134169712eb7762f02538ff99b11553
Sufyan03019777121 <156623479+Sufyan03019777121@users.noreply.github.com>
2025-05-16 09:30:07 +0500

    admin up118

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  timestamps: true, 
-
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // ✅ صحیح جگہ پر
+  }
+);
 
 module.exports = mongoose.model('User', userSchema);
