4aaa687711199874a1c0e8ea47fd233a1d6a206b
Sufyan03019777121 <156623479+Sufyan03019777121@users.noreply.github.com>
2025-07-15 22:15:23 +0500

    admin up182

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+import rateRoutes from './routes/rates.js';
 dotenv.config();
 
 // Route imports
@@ -11,7 +12,7 @@ const phoneRoutes = require('./routes/phoneRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const ordersRoute = require('./routes/orders');
-const rateRoutes = require('./routes/rates'); // merged route
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -32,7 +33,7 @@ app.use('/api/phoneNumbers', phoneRoutes);
 app.use('/api/contacts', contactRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', ordersRoute);
-app.use('/api/rates', rateRoutes); // merged
+app.use('/api/rates', rateRoutes); 
 
 // Start server
 app.listen(PORT, () => {
